d2fc3106b1e8b03920fd9b185905948d01f9f98f
erickMartinsSilva <erickmsilva19@gmail.com>
2026-05-20 20:12:11 -0300

    fix(routes): remover barras desnecessárias

--- api/src/routes/routes.go
+++ api/src/routes/routes.go
@@ -7,8 +7,8 @@ import (
 
 func SetupRoutes(){
 	http.HandleFunc("GET /users/{id}", controllers.GetUserById)
-	http.HandleFunc("GET /users/", controllers.GetUsers)
-	http.HandleFunc("POST /users/", controllers.CreateUser)
-	http.HandleFunc("PATCH /users/", controllers.UpdateUser)
+	http.HandleFunc("GET /users", controllers.GetUsers)
+	http.HandleFunc("POST /users", controllers.CreateUser)
+	http.HandleFunc("PATCH /users/{id}", controllers.UpdateUser)
 	http.HandleFunc("DELETE /users/{id}", controllers.DeleteUser)
 }
