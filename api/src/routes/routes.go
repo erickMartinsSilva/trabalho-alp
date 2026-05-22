72fbb4858430f0881b712f602c19a079038d3999
erickMartinsSilva <erickmsilva19@gmail.com>
2026-05-22 08:25:15 -0300

    feat(routes): endpoint de health

--- api/src/routes/routes.go
+++ api/src/routes/routes.go
@@ -1,11 +1,16 @@
 package routes
 
 import (
-	"net/http"
 	"ALP/src/controllers"
+	"ALP/src/utils"
+	"net/http"
 )
 
 func SetupRoutes(){
+	http.HandleFunc("GET /health", func(w http.ResponseWriter, r *http.Request) {
+		utils.SendResponse(w, http.StatusOK, map[string]any{ "status": "ok" }) 
+	})
+
 	http.HandleFunc("GET /users/{id}", controllers.GetUserById)
 	http.HandleFunc("GET /users", controllers.GetUsers)
 	http.HandleFunc("POST /users", controllers.CreateUser)
